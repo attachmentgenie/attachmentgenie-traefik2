13304881784fd2f23f7d30f703a3f40b9165a820
Bram Vogelaar <bram@attachmentgenie.com>
2020-06-30 23:16:15 +0200

    fixing test framework

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -6,6 +6,11 @@ describe 'traefik2::service' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:pre_condition) do
+        "class { 'traefik2':
+          static_config  => {},
+        }"
+      end
 
       it { is_expected.to compile }
     end
